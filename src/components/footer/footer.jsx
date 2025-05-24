797d86ca907022a57664c6c7247bfd02aa149e97
Štěpánka <Doma@Stepanka-MacBook-Air.local>
2025-05-24 10:39:40 +0200

    kod pro jsx a obrazek mapy

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+export const footer = () => (
+<section className="dark">
+    <div className="container columns-2">
+      <div className="columns">
+        <h2>Kontakt</h2>
+        <p>Hotel Stříbrava</p>
+        <p>Ke Kamenné lávce 12</p>
+        <p>317 24 Libnice nad Stříbravou</p>
+      </div>
+      <div className='email'>
+        <p>recepce@hotelstribrava.cz</p>
+      </div>
+      <img src="imgfooter/mapa.png" />
+    </div>
+</section>
+)
